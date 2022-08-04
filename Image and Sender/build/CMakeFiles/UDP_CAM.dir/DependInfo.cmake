6956c721398862e9a66e1ef04b0fbba89077ed21
Phillipdaw <77645015+Phillipdaw@users.noreply.github.com>
2022-08-04 10:26:22 +0100

    Add files via upload

---
+++ Image and Sender/build/CMakeFiles/UDP_CAM.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sommarjobbare/Facial-Recognition/udpcc/udp_streamer.cc" "/home/sommarjobbare/Facial-Recognition/udpcc/build/CMakeFiles/UDP_CAM.dir/udp_streamer.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/opencv4"
+  ".././sockpp/include"
+  ".././raspicam-0.1.9/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Image and Sender/build/CMakeFiles/UDP_CAM.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UDP_CAM.dir/udp_streamer.cc.o"
+  "UDP_CAM"
+  "UDP_CAM.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UDP_CAM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
